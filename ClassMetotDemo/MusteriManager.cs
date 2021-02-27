4785e690535e105a14feaf9d44aa37adcf867f4e
barissdal <dalbariss@gmail.com>
2021-02-27 22:16:31 +0300

    27 Şubat Ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Bilgileri Sisteme Eklendi:");
+            Console.WriteLine("Adı:" + musteri.Adi);
+            Console.WriteLine("Soyadı:" + musteri.Soyadi);
+            Console.WriteLine("Müsteri No:" + musteri.MusteriNo);
+            Console.WriteLine("Kredi Tutarı:" + musteri.KrediTutarı);
+        }
+
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müsteri Bilgileri Listeleniyor");
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " " + musteri.MusteriNo + " "  + 
+                    musteri.KrediTutarı + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteri1.Adi = "Barış";
+            musteri1.Soyadi = "Doğan";
+            musteri1.MusteriNo = 111111;
+            musteri1.KrediTutarı = 62000;
+
+
+            musteri2.Adi = "Barış";
+            musteri2.Soyadi = "Doğu";
+            musteri2.MusteriNo = 111111;
+            musteri2.KrediTutarı = 2000;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+
+            int islem = 0;
+            
+            Console.WriteLine("1-Müşterileri Ekleme.");
+            Console.WriteLine("2-Müşterileri Listeleme.");
+            Console.WriteLine("3-Müşterileri Silme.");
+            Console.WriteLine("4-Çıkış.");
+            
+
+
+            while(islem != 4)
+            {
+                Console.WriteLine("Bir işlem seçiniz.");
+                islem = Convert.ToInt16(Console.ReadLine());
+                if (islem == 1)
+                {
+                    musteriManager.MusteriEkle(musteri1);
+                    musteriManager.MusteriEkle(musteri2);
+                } 
+                else if(islem == 2)
+                {
+                    musteriManager.MusteriListeleme(musteriler);
+                }
+                else if( islem == 3)
+                {
+                    Console.WriteLine("Kaydınız silindi");
+                }
+                else
+                {
+                    Console.WriteLine("Teşekkür ederiz.");
+                }
+                
+            }
+
+        }
+    }
+}
