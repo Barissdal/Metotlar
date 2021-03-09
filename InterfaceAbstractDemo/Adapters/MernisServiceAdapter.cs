c291b2dffb99d2c2b8f66f6682e87872f036c6ee
barissdal <dalbariss@gmail.com>
2021-03-09 13:31:47 +0300

    09.03.2021 Study

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        // dış bir kaynaktan veri alıcaksak bunu bir Adapters'ta tanımlamak gerekmektedir.
+        public bool CheckIfRealPersonal(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap12);
+            //return client.TCKimlikNoDogrulaAsync(TCKimlikNo: customer.NationalityId,
+            //    Ad: customer.FirstName.ToUpper(),
+            //    Soyad: customer.LastName.ToUpper(),
+            //    DogumYili: customer.DateOfBirth.Year);
+
+            return client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId),
+                customer.FirstName.ToUpper(),
+                customer.LastName.ToLower(),
+                customer.DateOfBirth.Year).Result.Body.TCKimlikNoDogrulaResult;
+
+            //return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                NationalityId = "28861499108",
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                DateOfBirth = new DateTime(1988, 1, 6)
+
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
